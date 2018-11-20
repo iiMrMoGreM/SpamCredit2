e9244d59d1c39974e4076a78ff99c7d9daf9dd61
iiMrMoGreM <40735508+iiMrMoGreM@users.noreply.github.com>
2018-11-20 04:25:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "2say";
+var prefix = "2";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
